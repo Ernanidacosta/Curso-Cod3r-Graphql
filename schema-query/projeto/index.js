bf37e674649e13bd56234b89a5bb4458c3330ecd
Ernani <ernani.costa@vai.com.vc>
2022-06-19 12:43:58 -0300

    new scalar

---
+++ schema-query/projeto/index.js
@@ -0,0 +1,32 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+    scalar Date
+
+    # Entry point into the GraphQL API
+    type Query {
+        ola: String
+        horaAtual: Date
+    }
+`
+
+const resolvers = {
+    Query: {
+        ola() {
+            return 'Ola mundo!'
+        },
+        horaAtual() {
+            
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+})
+
