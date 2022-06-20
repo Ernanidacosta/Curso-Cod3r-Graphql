dbe89de82220311dbed59e7f39516ac599801db2
Ernani <ernani.costa@vai.com.vc>
2022-06-20 16:49:34 -0300

    desafio-produto-resposta

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -12,28 +12,47 @@ const typeDefs = gql`
         vip: Boolean
     }
 
+    type Produto {
+        id: ID!
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float
+    }
+
     # Entry point into the GraphQL API
     type Query {
         ola: String!
         dataHoraAtual: Date!
         usuarioLogado: Usuario
+        produtoDestaque: Produto
     }
 `
 
 const resolvers = {
     Usuario: {
         salario(usuario) {
-             return usuario.salario_real
+            return usuario.salario_real
         }
     },
 
-    
+    Produto: {
+        precoComDesconto(produto) {
+            if (produto.desconto) {
+                return produto.preco - (produto.desconto * produto.preco / 100)
+            } else {
+                return produto.preco
+            }
+        }
+    },
+
+
     Query: {
         ola() {
             return 'Ola mundo!'
         },
         dataHoraAtual() {
-            return new Date().toLocaleDateString() +' - '+ new Date().toLocaleTimeString()
+            return new Date().toLocaleDateString() + ' - ' + new Date().toLocaleTimeString()
         },
         usuarioLogado() {
             return {
@@ -44,7 +63,15 @@ const resolvers = {
                 salario_real: 3000.99,
                 vip: true
             }
-        }
+        },
+        produtoDestaque() {
+            return {
+                id: '1',
+                nome: 'Mouse',
+                preco: 200.00,
+                desconto: 50.00,
+            }
+        },
     }
 }
 
@@ -54,6 +81,6 @@ const server = new ApolloServer({
 })
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
+    console.log(`Server ready at ${url}`);
 })
 
